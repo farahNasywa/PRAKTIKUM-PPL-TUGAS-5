2577d390378f42fc50dde647c19370c2d1472bed
Farah Nasywa <166036176+farahNasywa@users.noreply.github.com>
2025-05-27 15:36:37 +0700

    Delete start directory

--- start/routes.ts
+++
@@ -1,7 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.get('/books', 'BooksController.index')
-Route.get('/books/:id', 'BooksController.show')
-Route.post('/books', 'BooksController.store')
-Route.put('/books/:id', 'BooksController.update')
-Route.delete('/books/:id', 'BooksController.destroy')
